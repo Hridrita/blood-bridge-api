e4f065b8f87fa4f9519a2c32a14502a511528619
Hridrita <moonsaha721@gmail.com>
2026-03-23 12:56:24 +0600

    import typeOr module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('Auth & Users') // এটি Swagger-এ সেকশন তৈরি করবে
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  @ApiOperation({ summary: 'Register a new donor or recipient' })
+  register(@Body() userData: any) {
+    return this.usersService.create(userData);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get all registered users (For Testing)' })
+  findAll() {
+    return this.usersService.findAll();
+  }
+}
