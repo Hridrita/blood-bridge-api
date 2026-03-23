57641daebcfaee712f5517dc21e142b79e18e071
Hridrita <moonsaha721@gmail.com>
2026-03-23 12:54:02 +0600

    added pass logic

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(userData: any): Promise<User> {
+    const { email, password } = userData;
+
+    const existingUser = await this.usersRepository.findOne({
+      where: { email },
+    });
+    if (existingUser) {
+      throw new ConflictException('Email already exists!');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = this.usersRepository.create({
+      ...userData,
+      password: hashedPassword,
+    });
+
+    return this.usersRepository.save(newUser);
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+}
