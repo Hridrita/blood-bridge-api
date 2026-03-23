88f5d4292fc2352050a4b9b16538d53470b6cb9d
Hridrita <moonsaha721@gmail.com>
2026-03-23 14:01:51 +0600

    modified user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Body, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
-import { CreateUserDto } from './dto/create-user.dto'; // এটি ইম্পোর্ট করা নিশ্চিত করুন
+import { CreateUserDto } from './dto/create-user.dto';
 
 @ApiTags('Auth & Users') 
 @Controller('users')
@@ -10,7 +10,6 @@ export class UsersController {
 
   @Post('register')
   @ApiOperation({ summary: 'Register a new donor or recipient' })
-  // নিচের লাইনে 'userData: any' এর বদলে 'createUserDto: CreateUserDto' লিখে দিলাম
   register(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
